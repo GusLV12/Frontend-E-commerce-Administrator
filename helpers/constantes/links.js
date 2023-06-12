6dc401c2466b4ee6215101c34f3c02a2cb3ab1b7
UrielMartinez21 <uriellmartinezz35@gmail.com>
2023-06-11 19:54:40 -0600

    Dropdown en campo categoria

--- helpers/constantes/links.js
+++ helpers/constantes/links.js
@@ -1,17 +1,17 @@
 //--> Crear usuario
-export const nuevoUsuario = "http://localhost:27017/api/administrador"
-export const validarToken = "http://localhost:27017/api/administrador/confirmar/"
+export const nuevoUsuario = "http://localhost:4000/api/administrador"
+export const validarToken = "http://localhost:4000/api/administrador/confirmar/"
 
 //--> Iniciar sesion
-export const iniciarSesion = "http://localhost:27017/api/administrador/iniSes"
+export const iniciarSesion = "http://localhost:4000/api/administrador/iniSes"
 
 //--> Resetear password
-export const resetearPassword = "http://localhost:27017/api/administrador/olvide-password"
-export const tokenResetearPassword = "http://localhost:27017/api/administrador/olvide-password/"
-export const cambiarPassword = "http://localhost:27017/api/administrador/olvide-password/"
+export const resetearPassword = "http://localhost:4000/api/administrador/olvide-password"
+export const tokenResetearPassword = "http://localhost:4000/api/administrador/olvide-password/"
+export const cambiarPassword = "http://localhost:4000/api/administrador/olvide-password/"
 
 //--> Funciones crud de catalogo 'producto'
-export const consultarProductos = "http://localhost:27017/api/productos/mostrarProductos"
-export const editarProducto = "http://localhost:27017/api/productos/modificarProducto"
-export const crearProducto = "http://localhost:27017/api/productos"
-export const eliminarProducto = "http://localhost:27017/api/productos/eliminarProducto"
+export const consultarProductos = "http://localhost:4000/api/productos/mostrarProductos"
+export const editarProducto = "http://localhost:4000/api/productos/modificarProducto"
+export const nuevoProducto = "http://localhost:4000/api/productos"
+export const eliminarProducto = "http://localhost:4000/api/productos/eliminarProducto"
