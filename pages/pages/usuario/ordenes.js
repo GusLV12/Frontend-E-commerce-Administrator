8340cf12a13fb856dfe966224095663e9132d340
Gabriela Campos <gabrielacampos1418@gmail.com>
2023-06-12 18:15:08 -0600

    o_g

--- pages/pages/usuario/ordenes.js
+++ pages/pages/usuario/ordenes.js
@@ -2,29 +2,23 @@ import React, { useEffect, useState, useRef } from "react";
 import Layout from "@/layout/layout"
 //--> Componentes primeReact
 import { Tag } from 'primereact/tag';
-import { classNames } from 'primereact/utils';
 import { Button } from 'primereact/button';
-import { FileUpload } from 'primereact/fileupload';
 import { Dialog } from 'primereact/dialog';
 import { InputText } from 'primereact/inputtext';
-import { InputNumber } from 'primereact/inputnumber';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import { Toast } from 'primereact/toast';
 import { Toolbar } from 'primereact/toolbar';
-import { Dropdown } from 'primereact/dropdown';
 import { RadioButton } from 'primereact/radiobutton';
-import { Message } from 'primereact/message';
+
+import { Chip } from 'primereact/chip';
 //--> Funciones propias
 import { objetoVacio } from "@/components/catalogos/objetovacio";
-
 import { formatoPrecio } from "@/helpers/funciones";
-import { camposVacios, descripcionInvalida, descuendoInvalido, nombreInvalido } from "@/helpers/constantes/mensajes";
-
 import { FormatoFecha } from "@/helpers/funciones";
 
 
-const Devoluciones = () => {
+const Ordenes = () => {
   //--> Estructura de objeto vacio
   let ordenVacia = objetoVacio
 
@@ -35,24 +29,22 @@ const Devoluciones = () => {
   //--> Dialogos
   const [deleteOrderDialog, setDeleteOrderDialog] = useState(false);
   const [deleteOrdersDialog, setDeleteOrdersDialog] = useState(false);
-  const [orderDialog, setOrderDialog] = useState(false);
   //--> Otros
   const [selectedOrders, setSelectedOrders] = useState(null);
-  const [submitted, setSubmitted] = useState(false);
   const [globalFilter, setGlobalFilter] = useState(null);
-  //--> Mensajes
-  const [mensajeRespuesta, setMensajeRespuesta] = useState('')
-    //-> Estatus Pedido
+  
+
+  //-> Estatus Pedido
    //---------- | Modificar Status | ------------
    const [displayDialog, setDisplayDialog] = useState(false);
    const [pedidoStatus, setPedidoStatus] = useState('');
    const [estatusOptions, setEstatusOptions] = useState([
      { label: 'Pendiente', value: 'Pendiente' },
-     { label: 'Procesada', value: 'Procesada' },
-     { label: 'Rechazada', value: 'Rechazada' }
+     { label: 'En camino', value: 'En camino' },
+     { label: 'Entregado', value: 'Entregado' }
    ]);
 
-
+  const [mensajeRespuesta, setMensajeRespuesta] = useState('')
   //--> Especiales
   const toast = useRef(null);
   const dt = useRef(null);
@@ -60,40 +52,49 @@ const Devoluciones = () => {
   //--> Cargar cuando se renderiza
   useEffect(() => {
     const datos = [
-      { idDev: 1, idProducto: 565, nomProducto: "Peluche Chimmy", estadoDev: "Pendiente", fechaDev : '20-06-2023', motivo: "Está roto", precioDev: 398.00,  idCliente: 15, nomCliente:'Omar Yu' , notas: "Lo quiero devolver porque ya no lo quiero."},
-      { idDev: 2, idProducto: 465, nomProducto: "Peluche Cooky Bebé",  estadoDev: "Pendiente",  fechaDev : '15-05-2023', motivo: "Está deforme", precioDev: 599.00,  idCliente: 18, nomCliente:'Lisa Mora' , notas: "No me gustó para regalar."},
-      { idDev: 3, idProducto: 12, nomProducto: "Rosetas primaverales", estadoDev: "Rechazada",  fechaDev : '03-06-2023',  motivo: "Llegarón maltratadas.", precioDev: 278.50,  idCliente: 136, nomCliente:'Lorena García' , notas: "..."},
-      { idDev: 4, idProducto: 68, nomProducto: "Jirafa jumbo",  estadoDev: "Procesada", fechaDev : '30-05-2023',  motivo: "Está descocido", precioDev: 800.99,  idCliente: 15, nomCliente:'Paola Campos' , notas: "..."},
-      { idDev: 5, idProducto: 645, nomProducto: "Delfín amistoso", estadoDev: "Pendiente", fechaDev : '01-04-2023',  motivo: "Color equivocado", precioDev: 215.00,  idCliente: 64, nomCliente:'Enrique Hernández' , notas: "..."},
-      { idDev: 6, idProducto: 31, nomProducto: "Tulipán", estadoDev: "Rechazada", fechaDev : '09-05-2023',  motivo: "Hojas secas", precioDev: 150.00,  idCliente: 353, nomCliente:'Juan Montes' , notas: "..."},
-     
-     
+      { idOrden: 115, idCliente: 145, nomCliente:'Omar Yu' , fechaRecepcion: "25-09-2023", 
+      productos: [{nombre: 'Arreglo Floral Girasoles', cantidad: 2}],  estadoOrden: 'En camino', direccionCliente: 'Av. Rosales', Total: 495},
+      
+      { idOrden: 116, idCliente: 1215, nomCliente:'Angelica Muñoz' , fechaRecepcion: "01-06-2023", 
+      productos: [{nombre: 'Arreglo Cumpleaños', cantidad: '3'},{nombre: 'Osito afelpado', cantidad: '2'}], 
+       estadoOrden: 'Entregado', direccionCliente: 'Av. Miguel Hidalgo', Total: 1895},
+      
+       { idOrden: 11655, idCliente: 1545, nomCliente:'Miguel Ontiveros' , fechaRecepcion: "20-06-2023", 
+      productos: [{nombre: 'Chimmy peluche', cantidad: '1'}],  estadoOrden: 'Pendiente', direccionCliente: 'Av. Rosales', Total: 256},
+      
+      { idOrden: 1215, idCliente: 1155, nomCliente:'Paulina Flores' , fechaRecepcion: "25-08-2023", 
+      productos: [{nombre: 'Cooky con hoddie', cantidad: '5'},{nombre: 'Flores rosas', cantidad: '1'},{nombre: 'Arreglo personalizado', cantidad: ''}],  estadoOrden: 'En camino', direccionCliente: 'Av. Sueños', Total: 3561},
     ]
     setOrders(datos)
   }, []);
 
-
   const getSeverity = (order) => {
-    switch (order.estadoDev) {
-      case 'Procesada': return 'success';
-      case 'Pendiente': return 'warning';
-      case 'Rechazada': return 'danger';
+    switch (order.estadoOrden) {
+      case 'Entregado': return 'success';
+      case 'En camino': return 'warning';
+      case 'En preparación': return 'danger';
       default: return null;
     }
   };
 
-  
-  const plantillaEstatus = (rowData) => {
-    return <Tag value={rowData.estadoDev} severity={getSeverity(rowData)}></Tag>;
-  };
-  //----------------| Interaccion con dialogos |----------------
- 
-  const cerrarDialogoEliminarRegistro = () => { setDeleteOrderDialog(false) };
-const cerrarDialogoEliminarRegistros = () => { setDeleteOrdersDialog(false) }
+  // --------- |Mostrar productos |----------
+  const plantillaProductos = (rowData) => {
+    return (
+      <div>
+        {rowData.productos.map((producto, index) => (
+          <Chip
+            key={index}
+            label={`Producto: ${producto.nombre} Cantidad: (${producto.cantidad})`}
+            className="p-mr-2"
+          />
+        ))}
+      </div>
+    );
+    }
 
 //------------- | Dialogo Estatus |-------- 
 
-const handleButtonClick = () => {
+  const handleButtonClick = () => {
     setDisplayDialog(true);
   };
 
@@ -101,10 +102,12 @@ const handleButtonClick = () => {
     setDisplayDialog(false);
   };
 
-  const handleStatusChange = (order) => {
+  const handleStatusChange = (e) => {
    
-    setOrder({ ...order });
-    setOrderDialog(true);
+    let _orden = { ... order };
+    
+        _orden['estadoOrden'] = e.value;
+        setPedidoStatus(_orden);
 
     toast.current.show({
       severity: 'success', summary: 'Estatus Guardado', detail: 'Se ha actualizado correctamente el estatus del pedido', life: 3000
@@ -113,52 +116,42 @@ const handleButtonClick = () => {
        
     setDisplayDialog(false);
   };
+  
+
+ // --------- |Plantillas| -------------
+
+  const plantillaEstatus = (rowData) => {
+    return <Tag value={rowData.estadoOrden} severity={getSeverity(rowData)}></Tag>;
+  };
+  const plantillaPrecio = (rowData) => { return formatoPrecio(rowData.Total) }
+  const fecha = (rowData) => {return FormatoFecha(rowData.fechaRecepcion) }
+
+  //----------------| Interaccion con dialogos |----------------
+ 
+  const cerrarDialogoEliminarRegistro = () => { setDeleteOrderDialog(false) };
+
+  const cerrarDialogoEliminarRegistros = () => { setDeleteOrdersDialog(false) }
+
   //----------------| Funciones Back-end |----------------
  
     //--> Editar registro
    
-    const botonesCrearModificar = (
-        <>
-          <Button security="success" label="Guardar" severity="success" icon="pi pi-check"  />
-          <Button security="danger" label="Cancelar"  severity="danger" icon="pi pi-times"x />
-        </>
-      );
-      const cambiarEstatus = (e) => {
-        let _order = { ...order };
-    
-        _order ['estadoDev'] = e.value;
-        setOrder(_order);
-      };
+
 
   const confirmarEliminarRegistro = (order) => {
     setOrder(order);
     setDeleteOrderDialog(true);
   };
 
-  const abrirDialogoCM = () => {
-    setOrder(ordenVacia);
-    setSubmitted(false);
-    setOrderDialog(true);
-  };
-  const cerrarDialogoCM = () => {
-    setSubmitted(false);
-    setOrderDialog(false);
-  };
-  
-//   const editarRegistro = (order) => {
-//     setOrder({ ...order });
-//     setOrderDialog(true);
-//   };
-
   const eliminarRegistro = () => {
     //--> Registros que no sean los seleccionados
-    let _orders = orders.filter((val) => val.id !== order.idDev);
+    let _orders = orders.filter((val) => val.id !== order.id);
 
     setOrders(_orders);
     setDeleteOrderDialog(false);
     setOrder(ordenVacia);
     toast.current.show({
-      severity: 'success', summary: 'Registro(s)  de cancelación eliminado(s)', detail: 'Se ha eliminado correctamente el registro seleccionado.', life: 3000
+      severity: 'success', summary: 'Registro(s) de ordenes eliminado', detail: 'Se ha eliminado correctamente el registro seleccionado', life: 3000
     });
   };
 
@@ -173,17 +166,15 @@ const handleButtonClick = () => {
     setDeleteOrdersDialog(false);
     setSelectedOrders(null);
     toast.current.show({
-      severity: 'success', summary: 'Registro(s) de cancelación eliminado(s)', detail: 'Se ha eliminado correctamente el registro seleccionado', life: 3000
+      severity: 'success', summary: 'Registro(s) de ordenes eliminado', detail: 'Se ha eliminado correctamente el registro seleccionado', life: 3000
     });
   };
 
-  const plantillaPrecio = (rowData) => { return formatoPrecio(rowData.precioDev) }
-  const fecha = (rowData) => {return FormatoFecha(rowData.fechaDev) }
   
   //----------------| Botones de dialogos |----------------
   const cabezal = (
     <div className="flex flex-wrap gap-2 align-items-center justify-content-between">
-      <h4 className="m-0">Registro de Devoluciones</h4>
+      <h4 className="m-0">Registro de Órdenes</h4>
       <span className="p-input-icon-left">
         <i className="pi pi-search" />
         <InputText type="search" onInput={(e) => setGlobalFilter(e.target.value)} placeholder="Buscar..." />
@@ -196,6 +187,7 @@ const handleButtonClick = () => {
       <div className="flex flex-wrap gap-2">
       
         <Button label="Eliminar" icon="pi pi-trash" severity="danger" onClick={confirmDeleteSelected} disabled={!selectedOrders || !selectedOrders.length} />
+       
       </div>
     );
   };
@@ -204,10 +196,12 @@ const handleButtonClick = () => {
   const botonesAccion = (rowData) => {
     return (
       <>
-        <Button icon="pi pi-check-square"  severity="success" label="Validar" onClick={handleButtonClick}    /> 
-        
-        {/* onClick={() => {() => editarRegistro(rowData)}} */}
-        <Button icon="pi pi-trash" severity="danger" onClick={() => confirmarEliminarRegistro(rowData)} label="Eliminar" />
+       <Button label="Estatus"
+       icon= 'pi pi-pencil'
+        severity="success"
+        onClick={handleButtonClick}
+      />
+        <Button icon="pi pi-trash" label="Eliminar"  severity="danger" onClick={() => confirmarEliminarRegistro(rowData)} />
       </>
     );
   };
@@ -232,12 +226,11 @@ const handleButtonClick = () => {
   //----------------| Valor que regresara |----------------
   return (
     <Layout
-      title="Cancelaciones"
-      description="Acceso al registro de cancelaciones"
+      title="órdenes"
+      description="Acceso al registro de Ordenes"
     >
       <div className="grid">
         <Toast ref={toast} />
-        
         <div className="col-12">
           <div className="card">
             <Toolbar className="mb-4" right={deleteButton} />
@@ -250,49 +243,33 @@ const handleButtonClick = () => {
               globalFilter={globalFilter} header={cabezal}
             >
               <Column selectionMode="multiple" exportable={false} />
-              <Column field="idDev" header="ID Devolución" sortable style={{ minWidth: '12rem', textAlign: "center" }} />
-              <Column field="idProducto" header="ID Producto" sortable style={{ minWidth: '10rem', textAlign: "center" }} />
-              <Column field="nomProducto" header="Nombre del Producto" sortable style={{ minWidth: '16rem', textAlign: "center" }} />
-              <Column field="fechaDev" header="Fecha de Devolución"  sortable style={{ minWidth: '16rem', textAlign: "center" }}> <FormatoFecha fechaDev={fecha} /> </Column>
-              <Column field="motivo" header="Motivo" sortable style={{ minWidth: '16rem', textAlign: "center" }} />
-              <Column field="precioDEV" header="Cantidad a Devolver" body={plantillaPrecio}  sortable style={{ minWidth: '16rem', textAlign: "center" }} />
-              <Column field="estadoDev" header="Estado de la Devolución"   body={plantillaEstatus} sortable style={{ minWidth: '16rem', textAlign: "center" }} />
-              <Column field="idCliente" header="ID Cliente" sortable style={{ minWidth: '10rem', textAlign: "center" }} />
+              <Column field="idOrden" header="ID Orden" sortable style={{ minWidth: '12rem', textAlign: "center" }} />
+              <Column field="idCliente" header="ID Cliente" sortable style={{ minWidth: '12rem', textAlign: "center" }} />
               <Column field="nomCliente" header="Nombre Cliente" sortable style={{ minWidth: '16rem', textAlign: "center" }} />
-              
-              <Column header="Validar / Eliminar registro" body={botonesAccion} exportable={false} style={{ minWidth: '20rem' , textAlign: "center"}} />
+              <Column field="fechaRecepcion" header="Fecha de Recepción" sortable style={{ minWidth: '16rem', textAlign: "center" }} > <FormatoFecha fechaRecepcion={fecha} /> </Column>
+              <Column  header="Productos"  body={plantillaProductos} sortable style={{ minWidth: '12rem', textAlign: "center" }} />
+           
+       
+              <Column field="estadoOrden" header="Estatus" body={plantillaEstatus} sortable style={{ minWidth: '12rem', textAlign: "center" }} />
+              <Column field="direccionCliente" header="Dirección" sortable style={{ minWidth: '16rem', textAlign: "center" }} />
+              <Column field="Total" header="Total"  body={plantillaPrecio} sortable style={{ minWidth: '12rem', textAlign: "center" }} />
+             
+              <Column header="Modificar Estatus / Eliminar registro" body={botonesAccion} exportable={false} style={{ minWidth: '20rem' , textAlign: "center"}} />
             </DataTable>
 
             <Dialog
         visible={displayDialog}
-        style={{ width: '32rem' }} breakpoints={{ '960px': '75vw', '641px': '90vw' }}
-        modal className="p-fluid" 
         onHide={handleDialogHide}
         header="Modificar estado del pedido"
         footer={
           <div>
             <Button label="Guardar" severity="success" onClick={handleStatusChange} autoFocus />
-            <Button label="Cancelar" severity="danger" onClick={handleDialogHide}  />
+            <Button label="Cancelar" severity="danger" onClick={handleDialogHide} className="p-button-text" />
             
           </div>
         }
       >
         <div>
-       
-        <div className="field">
-                <label htmlFor="nombre" className="font-bold">Nombre del Cliente:  {order.nomCliente} </label> <br/> <br/>
-                 <label htmlFor="nombre" className="font-bold">Producto a Devolver: </label>
-                <label> {order.idProducto} </label> <br/> <br/>
-                 <label htmlFor="idCliente" className="font-bold">Motivo: </label>
-                 <label> {order.motivo} </label> <br/> <br/>
-                 <label htmlFor="nombre" className="font-bold">Cantidad a Devolver:  </label>
-                <label body={plantillaPrecio}> ${order.precioDev} MXN  </label> <br/> <br/>
-                 <label htmlFor="idCliente" className="font-bold">Notas: </label>
-                 <label> {order.notas} </label> <br/>
-                
-               
-              </div>
-
           <h5>Nuevo estado del pedido:</h5> <br/>
           {estatusOptions.map((option) => (
             <div key={option.value}>
@@ -309,12 +286,9 @@ const handleButtonClick = () => {
         </div>
       </Dialog>
 
-
-         
-
             <Dialog
               visible={deleteOrderDialog} style={{ width: '32rem' }}
-              breakpoints={{ '960px': '75vw', '641px': '90vw' }} header="Confirm" modal footer={botonesEliminarRegistro}
+              breakpoints={{ '960px': '75vw', '641px': '90vw' }} header="Eliminar Registro" modal footer={botonesEliminarRegistro}
               onHide={cerrarDialogoEliminarRegistro}
             >
               <div className="confirmation-content">
@@ -340,4 +314,4 @@ const handleButtonClick = () => {
   )
 }
 
-export default Devoluciones
+export default Ordenes
